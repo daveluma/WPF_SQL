126e62c8281986f1e67dbe991abffea766cee077
unknown <daveluma@gmail.com>
2020-10-26 21:41:33 -0400

    Adding WPF_SQL

---
+++ WPF_SQL_UI/WPF_SQL_UI/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data.Sql;
+using System.Data.SqlClient;
+
+namespace WPF_SQL_UI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        SqlCommand cmd;
+        SqlConnection con;
+        SqlDataAdapter da;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SubmitButton_Click(object sender, RoutedEventArgs e)
+        {
+            con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Owner\Documents\NabaBase.mdf;Integrated Security=True;Connect Timeout=30");
+            con.Open();
+            cmd = new SqlCommand("INSERT INTO FirstLast (FirstName,LastName) VALUES (@FirstName,@LastName)", con);
+            cmd.Parameters.Add("@FirstName", FirstNameBox.Text);
+            cmd.Parameters.Add("@LastName", LastNameBox.Text);
+            cmd.ExecuteNonQuery();
+        }
+    }
+}
